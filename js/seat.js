feb2afff613693027092abb579bc16974f8d068a
unknown <tin@e.com>
2014-02-22 17:15:53 +0800

    add some shit

--- js/seat.js
+++ js/seat.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
-  var stage = new Kinetic.Stage({
-    container: 'container',
+  var stage_small = new Kinetic.Stage({
+    container: 'smallcontainer',
     width: 320,
-    height: 320
+    height: 400
   });
 
-  var layer = new Kinetic.Layer();
+  var stage_big = new Kinetic.Stage({
+    container: 'bigcontainer',
+    width: 320,
+    height: 400
+  });
+
+  var layer_small = new Kinetic.Layer();
+  var layer_big = new Kinetic.Layer();
 
   var widthIndex = 32;
   var heightIndex = 32;
@@ -23,7 +30,33 @@ $(document).ready(function () {
     name: 'rect0'
   });
 
-  for(var i=0;i<2;i++){
+  var rect_small = new Kinetic.Rect({
+    x: 0,
+    y: 0,
+    width: 320,
+    height: 400,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4,
+    // draggable: true,
+    opacity: 0.9,
+    name: 'rectsmall'
+  });
+
+  var rect_big = new Kinetic.Rect({
+    x: 0,
+    y: 0,
+    width: 320,
+    height: 400,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4,
+    // draggable: true,
+    opacity: 0.5,
+    name: 'rectbig'
+  });
+
+  for(var i=0;i<30;i++){
     var rect1 = new Kinetic.Rect({
       x: 0+widthIndex*i,
       y: 0+heightIndex*i,
@@ -34,21 +67,44 @@ $(document).ready(function () {
       strokeWidth: 4,
       name: 'rect1'
     });
-
       // add the shape to the layer
-      layer.add(rect1);
+      layer_small.add(rect1);
     }
+
+    for(var i=0;i<30;i++){
+      var rect1_big = new Kinetic.Rect({
+        x: 0+widthIndex*i,
+        y: 0+heightIndex*i,
+        width: 32,
+        height: 32,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 4,
+        name: 'rect1_big'
+      });
+      // add the shape to the layer
+      layer_big.add(rect1_big);
+    }
+
     var group = new Kinetic.Group({
       draggable: true
     });
     group.add(rect0);
-    layer.add(group);
 
+    layer_small.add(rect_small);
+    layer_small.add(group);
+
+    layer_big.add(rect_big);
+    var magnifyingposition_x=0
+    var magnifyingposition_y=0
     group.on('dragend',function(){
-      console.log("x"+group.getPosition().x+" y"+group.getPosition().y);
+      magnifyingposition_x = group.getPosition().x;
+      magnifyingposition_y = group.getPosition().y;
+      console.log("x"+magnifyingposition_x+" y"+magnifyingposition_y);
     });
       // add the layer to the stage
-      stage.add(layer);
+      stage_small.add(layer_small);
+      stage_big.add(layer_big);
     });
 
 
