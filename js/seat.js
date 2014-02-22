751cc1f4849ec2fed309fd966c21457c93f4b55c
unknown <tin@e.com>
2014-02-22 15:53:55 +0800

    check seat.js

--- js/seat.js
+++ js/seat.js
@@ -1,41 +1,54 @@
 $(document).ready(function () {
-      var stage = new Kinetic.Stage({
-        container: 'container',
-        width: 320,
-        height: 480
-      });
+  var stage = new Kinetic.Stage({
+    container: 'container',
+    width: 320,
+    height: 320
+  });
 
-      var layer = new Kinetic.Layer();
+  var layer = new Kinetic.Layer();
 
-      var widthIndex = 32;
-      var heightIndex = 32;
+  var widthIndex = 32;
+  var heightIndex = 32;
 
-      var rect0 = new Kinetic.Rect({
-        x: 0,
-        y: 0,
-        width: 32,
-        height: 32,
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 4
-        name: 'rect0'
-      });
+  var rect0 = new Kinetic.Rect({
+    x: 0,
+    y: 0,
+    width: 160,
+    height: 160,
+    fill: '#ffffff',
+    stroke: 'black',
+    strokeWidth: 4,
+    // draggable: true,
+    opacity: 0.3,
+    name: 'rect0'
+  });
 
-      var rect1 = new Kinetic.Rect({
-        x: 0+widthIndex,
-        y: 0+heightIndex,
-        width: 32,
-        height: 32,
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 4
-        name: 'rect1'
-      });
+  for(var i=0;i<2;i++){
+    var rect1 = new Kinetic.Rect({
+      x: 0+widthIndex*i,
+      y: 0+heightIndex*i,
+      width: 32,
+      height: 32,
+      fill: 'green',
+      stroke: 'black',
+      strokeWidth: 4,
+      name: 'rect1'
+    });
 
       // add the shape to the layer
-      layer.add(rect0);
       layer.add(rect1);
+    }
+    var group = new Kinetic.Group({
+      draggable: true
+    });
+    group.add(rect0);
+    layer.add(group);
 
+    group.on('dragend',function(){
+      console.log("x"+group.getPosition().x+" y"+group.getPosition().y);
+    });
       // add the layer to the stage
       stage.add(layer);
-});
+    });
+
+
