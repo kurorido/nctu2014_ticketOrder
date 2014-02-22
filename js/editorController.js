b3dbac2f3a5c052e68b9464e932e77b0ec7046cf
kurorido <kuroridoplayer@gmail.com>
2014-02-23 00:53:21 +0800

    fix price bug

---
+++ js/editorController.js
@@ -0,0 +1,38 @@
+      function addSeatType(addFromTemplate) {
+
+        addFromTemplate = addFromTemplate || false;
+
+        if(!addFromTemplate) {
+          $( "#color-mapping-list" ).append( "<li><div style=\"display: inline-block\" onclick=\"changeType("+ (seatTypeCount) + ")\"><div style=\"height:15px; width: 15px; background-color:" + modeColor[seatTypeCount] +"; display: inline-block\"></div><span value="+ seatTypeCount +" style=\"padding-left: 10px;\"><span onclick=\"changeText(this)\">New Ticket Type</span></span><span style=\"padding-left:10px\"><span onclick=\"changePrice(this)\">300</span></span></div></li>" );
+          modeName.push('New Ticket Type');
+        } else {
+          if(seatTypeCount != 0) {
+           $( "#color-mapping-list" ).append( "<li><div style=\"display: inline-block\" onclick=\"changeType("+ (seatTypeCount) + ")\"><div style=\"height:15px; width: 15px; background-color:" + modeColor[seatTypeCount] +"; display: inline-block\"></div><span value="+ seatTypeCount +" style=\"padding-left: 10px;\"><span onclick=\"changeText(this)\">"+ modeName[seatTypeCount] + "</span></span><span style=\"padding-left:10px\"><span onclick=\"changePrice(this)\">" + modePrice[seatTypeCount] +"</span></div></li>" );
+         } else {
+            $( "#color-mapping-list" ).append( "<li><div style=\"display: inline-block\" onclick=\"changeType("+ (seatTypeCount) + ")\"><div style=\"height:15px; width: 15px; background-color:" + modeColor[seatTypeCount] +"; display: inline-block\"></div><span value="+ seatTypeCount +" style=\"padding-left: 10px;\"><span onclick=\"changeText(this)\">"+ modeName[seatTypeCount] + "</span></div></li>" );         
+         }
+        }
+        seatTypeCount++;
+      }
+
+
+
+      function changeText(target) {
+        target.parentNode.innerHTML = "<input type='text' value='" + target.innerHTML  + "'/><button onclick='textOK(this.parentNode)'>OK</button>";
+      }
+
+      function changePrice(target) {
+        target.parentNode.innerHTML = "<input type='text' value='" + target.innerHTML  + "'/><button onclick='priceOK(this.parentNode)'>OK</button>";       
+      }
+
+      function textOK(target) {
+        var newValue = target.firstChild.value;
+        target.innerHTML = "<span onclick=\"changeText(this)\">" + newValue + "</span>";
+        modeName[target.getAttribute('value')] = newValue;
+      }
+
+      function priceOK(target) {
+        var newValue = target.firstChild.value;
+        target.innerHTML = "<span onclick=\"changePrice(this)\">" + newValue + "</span>";
+        //modeName[target.getAttribute('value')] = newValue;       
+      }
