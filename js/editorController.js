347e91a5e744273656d3be8a84df51940d6b94f6
kurorido <kuroridoplayer@gmail.com>
2014-02-23 03:59:15 +0800

    update

--- js/editorController.js
+++ js/editorController.js
@@ -37,3 +37,23 @@
         target.innerHTML = "<span onclick=\"changePrice(this)\">" + newValue + "</span>";
         modePrice[target.getAttribute('value')] = newValue;
       }
+
+function setCookie(cname,cvalue,exdays)
+{
+var d = new Date();
+d.setTime(d.getTime()+(exdays*24*60*60*1000));
+var expires = "expires="+d.toGMTString();
+document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname)
+{
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) 
+  {
+  var c = ca[i].trim();
+  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+}
+return "";
+}
